e726bccc448de36420f74bc775e80664adb8785c
Agredas <andreagredas@gmail.com>
2021-01-04 11:24:43 +0100

    Arwes installed.

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -1,10 +1,13 @@
+import { Content } from "arwes";
 import React from "react";
 import "./Catalog.scss";
 
 function Catalog(props) {
   return (
     <div style={props.content?.rows.length ? {display: 'block'} : {display: 'none'}} className={`catalogContent`}>
-      <h2 className="catalogTitle">{props.title}</h2>
+      <Content>
+        <h2 className="catalogTitle">{props.title}</h2>
+      </Content>
     </div>
   );
 }
