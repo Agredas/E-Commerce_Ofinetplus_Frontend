1f8a1a620d9756afbd8e53fafe3c713bea3e85e7
Agredas <andreagredas@gmail.com>
2021-01-04 11:42:06 +0100

    Component Search created.

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from 'react';
+import { Button } from 'arwes';
+import './SearchFilm.scss';
+import MagnifyIcon from 'mdi-react/MagnifyIcon';
+
+
+const Search = (props) => {
+
+  useEffect(() => {
+    try {
+
+
+    } catch (err) {
+      console.log(err);
+    }
+  }, []);
+  const [search, setSearch] = useState("");
+
+  const eventHandler = (ev) => {
+    setSearch(ev.target.value)
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      axios.post('http://localhost:8000/api/product/name/{name}')
+    } catch (error) {
+      console.log(error.message)
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="field-search">
+        <div className="control">
+          <input
+            className="inputSearch"
+            onChange={eventHandler}
+            type="text"
+          />
+        </div>
+        <div className="control">
+          <Button className="button is-info" >
+            <MagnifyIcon className='verticalAlignIcons' />Buscador
+          </Button>
+        </div>
+      </div>
+    </form>
+  )
+}
+
+export default Search;
