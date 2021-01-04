6ee16718d6c8521d9376537cf7c369277cd3f226
Agredas <andreagredas@gmail.com>
2021-01-04 12:07:44 +0100

    Starting with the product view.

---
+++ src/containers/Store/Store.jsx
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import Catalog from "../../components/Catalog/Catalog";
+import Footer from "../../components/Footer/Footer";
+import "./Store.scss";
+import Header from "../../components/Header/Header";
+import { useEffect, useState } from "react";
+import Search from '../../components/Search/Search';
+
